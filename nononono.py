c1657711efee64ce3fbf1655fc9cfedafe7bfdd1
philip-mv <philipmv10@gmail.com>
2025-01-23 16:13:05 +0530

    k bdsoboi

---
+++ nononono.py
@@ -0,0 +1,14 @@
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+a = "Pinapple"
+b = "Chaapal"
+print(a != b)
+a = 4
+b = 5
+if (a == 1) != (b == 5) :
+    print("hello")
+else :
+    print("bye")
