abf95d0159512488e7b195cb9d963d9af399b555
philip-mv <philipmv10@gmail.com>
2025-01-23 16:15:07 +0530

    lijjlik

--- nononono.py
+++ nononono.py
@@ -12,3 +12,8 @@ if (a == 1) != (b == 5) :
     print("hello")
 else :
     print("bye")
+a = int(input("Please enter a number : "))
+if a%2 != 0:
+    print("the number is odd.")
+else :
+    print("The number is even.")
